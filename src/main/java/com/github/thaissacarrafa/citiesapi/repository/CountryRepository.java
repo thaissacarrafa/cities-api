5c9ed04b010753f6c3e5ded088bf52fc9707d22e
Thaissa Carrafa <thaissacarrafa@gmail.com>
2021-11-12 17:35:24 -0300

    Startup na API

---
+++ src/main/java/com/github/thaissacarrafa/citiesapi/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.github.thaissacarrafa.citiesapi.repository;
+
+
+import com.github.thaissacarrafa.citiesapi.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+
+}
